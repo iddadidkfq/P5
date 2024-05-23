3ecac4f40508f60813d72a7406bf2ed047e59e50
Jeff <65812802+iddadidkfq@users.noreply.github.com>
2024-05-23 12:23:51 +0200

    Mise en place du panier (local storage)

--- front/js/product.js
+++ front/js/product.js
@@ -7,7 +7,7 @@ async function main() {
      * Fonction principale de index.html - 
      * mis en fonction pour pouvoir être en async sur lireProduits()
      */
-
+ /**    clearLocalStorage () */
     /**recuperation product ID  */
 
     const url = new URL(window.location.href);
@@ -39,15 +39,68 @@ async function main() {
     /** MAJ couleurs */
 
     let myColors = product.colors
+    let choixCouleur = document.getElementById ("colors")
 
     for (let i = 0; i < myColors.length; i++) {
         const baliseOption = document.createElement("option");
         baliseOption.value= myColors[i]
         baliseOption.innerText = myColors[i]
-        let choixCouleur = document.getElementById ("colors")
         choixCouleur.appendChild(baliseOption)
     }
 
+    let maCouleur="" 
+    choixCouleur.addEventListener("change", function () {
+        maCouleur = choixCouleur.options[choixCouleur.selectedIndex].text;
+        console.log(maCouleur)
+    })
+
+    /**MAJ Quantité */
+    let maQuantite=1
+    let choixQuantite=document.getElementById ("quantity")
+    choixQuantite.value=1
+    choixQuantite.addEventListener("change", function () {
+        maQuantite = Number(choixQuantite.value);
+        console.log(maQuantite)
+    })
+    /**Clic bouton panier */
+
+    let monBouton = document.getElementById("addToCart")
+    monBouton.addEventListener("click",function () {
+        if ((ctrlQuantite(choixQuantite.value) === true) && 
+                (ctrlCouleur(maCouleur) === true)){   
+            logLocalStorage()
+            key = id + "-" + maCouleur
+            let currentQuantity = Number(window.localStorage.getItem(key))
+            console.log (currentQuantity)
+            if (currentQuantity === null){
+                currentQuantity = maQuantité
+            } else {
+                currentQuantity = currentQuantity + maQuantite
+            }
+            window.localStorage.setItem(key, currentQuantity)
+            logLocalStorage()
+        }
+    })
+}   
+
+function ctrlQuantite(valeur){
+        let regex = new RegExp("^[1-9][0-9]?$")
+    if (regex.test(valeur)===false){
+        alert("La quantité " + valeur + " n'est pas valide")
+        return (false)
+    } else {
+        return (true)
+    }   
+}
+
+function ctrlCouleur(valeur){
+    if ((valeur === "--SVP, choisissez une couleur --") |
+        (valeur === "")) {
+        alert("Veuillez saisir une couleur")
+        return (false)
+    } else {
+        return (true)
+    }   
 }
 async function lireProduit(id) {
 
@@ -62,4 +115,12 @@ async function lireProduit(id) {
    
 }
 
+function logLocalStorage () {
+    for (var i = 0; i < localStorage.length; i++){
+        console.log (localStorage.key(i) + "=" + localStorage.getItem(localStorage.key(i)))
+    }
+}
 
+function clearLocalStorage () {
+    localStorage.clear()
+}
